bc51c880a2e46b66281b56bb1584860b88f435fd
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-06-24 20:57:38 -0400

    Add the contact call to action component

---
+++ components/contact-cta.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const ContactCta = () => {
+  return (
+    <div className='contact-cta'>
+      <h2 className='contact-cta__headline'>
+        Interested in doing a project together?
+      </h2>
+      <hr className='contact-cta__rule' />
+      <Link href='/contact'>
+        <a className='button contact-cta__button'>Contact Me</a>
+      </Link>
+    </div>
+  )
+}
+
+export default ContactCta
