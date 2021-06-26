5984c9d4d8262c49d6051d62461b499160f9a965
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-06-26 18:59:19 -0400

    Add form error state handling

---
+++ components/ContactForm.jsx
@@ -0,0 +1,230 @@
+import React, { useReducer, useState } from 'react'
+
+const initialState = {
+  fullname: { value: '', touched: false, hasError: false, error: '' },
+  email: { value: '', touched: false, hasError: false, error: '' },
+  message: { value: '', touched: false, hasError: false, error: '' },
+  isFormValid: false,
+}
+
+const UPDATE_FORM = 'UPDATE_FORM'
+const RESET_FORM = 'RESET_FORM'
+
+const validateInput = (name, value) => {
+  let hasError = false
+  let error = ''
+
+  switch (name) {
+    case 'fullname':
+      if (value.trim() === '') {
+        hasError = true
+        error = 'This field is required'
+      }
+      break
+      case 'email':
+        const emailRegEx = new RegExp(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/)
+        if (value.trim() === '') {
+        hasError = true
+        error = 'This field is required'
+      } else if (!emailRegEx.test(value)) {
+        hasError = true
+        error = 'Please use a valid email address'
+      }
+      break
+    case 'message':
+      if (value.trim() === '') {
+        hasError = true
+        error = 'This field is required'
+      }
+      break
+    default:
+      break
+  }
+
+  return { hasError, error }
+}
+
+const onInputChange = (name, value, dispatch, formState) => {
+  const { hasError, error } = validateInput(name, value)
+  let isFormValid = true
+
+  for (const key in formState) {
+    const item = formState[key]
+
+    if (key === name && hasError) {
+      isFormValid = false
+      break
+    } else if (key !== name && item.hasError) {
+      isFormValid = false
+      break
+    }
+  }
+
+  dispatch({
+    type: UPDATE_FORM,
+    data: {
+      name,
+      value,
+      hasError,
+      error,
+      touched: false,
+      isFormValid,
+    },
+  })
+}
+
+const onFocusOut = (name, value, dispatch, formState) => {
+  const { hasError, error } = validateInput(name, value)
+  let isFormValid = true
+
+  for (const key in formState) {
+    const item = formState[key]
+    if (key === name && hasError) {
+      isFormValid = false
+      break
+    } else if (key !== name && item.hasError) {
+      isFormValid = false
+      break
+    }
+  }
+
+  dispatch({
+    type: UPDATE_FORM,
+    data: { name, value, hasError, error, touched: true, isFormValid },
+  })
+}
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_FORM:
+      const { name, value, hasError, error, touched, isFormValid } = action.data
+      return {
+        ...state,
+        [name]: { ...state[name], value, hasError, error, touched },
+        isFormValid,
+      }
+    case RESET_FORM:
+      return action.data
+    default:
+      return state
+  }
+}
+
+export default function ContactForm() {
+  const [formState, dispatch] = useReducer(formReducer, initialState)
+  const [showError, setShowError] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    let isFormValid = true
+
+    for (const name in formState) {
+      const item = formState[name]
+      const { value } = item
+      const { hasError, error } = validateInput(name, value)
+
+      if (hasError) {
+        isFormValid = false
+      }
+
+      if (name) {
+        dispatch({
+          type: UPDATE_FORM,
+          data: { name, value, hasError, error, touched: true, isFormValid },
+        })
+      }
+    }
+
+    if (!isFormValid) {
+      setShowError(true)
+    } else {
+      dispatch({
+        type: RESET_FORM,
+        data: initialState
+      })
+    }
+  }
+
+  return (
+    <>
+      {showError && !formState.isFormValid && (
+        <div className="form__error">There was a problem with your entries.</div>
+      )}
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="form__control">
+          <label htmlFor="fullname">Name</label>
+          <input
+            className={"form__input " + (formState.fullname.hasError ? "error" : "")}
+            type="text"
+            name="fullname"
+            placeholder="Jane Appleseed"
+            id="fullname"
+            autoComplete="name"
+            aria-required="true"
+            required
+            onChange={(e) => {
+              onInputChange('fullname', e.target.value, dispatch, formState)
+            }}
+            onBlur={(e) => {
+              onFocusOut('fullname', e.target.value, dispatch, formState)
+            }}
+            value={formState.fullname.value}
+          />
+          {formState.fullname.touched && formState.fullname.hasError && (
+            <div className="error">{formState.fullname.error}</div>
+          )}
+        </div>
+        <div className="form__control">
+          <label htmlFor="email">Email Address</label>
+          <input
+            className={"form__input " + (formState.email.hasError ? "error" : "")}
+            type="email"
+            name="email"
+            placeholder="email@example.com"
+            id="email"
+            autoComplete="email"
+            aria-required="true"
+            required
+            onChange={(e) => {
+              onInputChange('email', e.target.value, dispatch, formState)
+            }}
+            onBlur={(e) => {
+              onFocusOut('email', e.target.value, dispatch, formState)
+            }}
+            value={formState.email.value}
+          />
+          {formState.email.touched && formState.email.hasError && (
+            <div className="error">{formState.email.error}</div>
+          )}
+        </div>
+        <div className="form__control">
+          <label htmlFor="message">Message</label>
+          <textarea
+            className={"form__input " + (formState.message.hasError ? "error " : "")}
+            name="message"
+            id="message"
+            cols="20"
+            rows="3"
+            placeholder="How can I help?"
+            aria-required="true"
+            required
+            onChange={(e) => {
+              onInputChange('message', e.target.value, dispatch, formState)
+            }}
+            onBlur={(e) => {
+              onFocusOut('message', e.target.value, dispatch, formState)
+            }}
+            value={formState.message.value}
+          />
+          {formState.message.touched && formState.message.hasError && (
+            <div className="error">{formState.message.error}</div>
+          )}
+        </div>
+        <button type="submit" className="button button-primary">
+          Send Message
+        </button>
+      </form>
+    </>
+  )
+}
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,18 +3,19 @@ import Image from 'next/image'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
+import ContactForm from '../components/ContactForm'
 
-export default function Home () {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Frontend Mentor | Minimalist Portfolio</title>
       </Head>
       <Header />
-      <main id='main-content' tabIndex='-1' className='wrapper'>
-        <section className='contact-description'>
-          <h2 className='contact-description__headline'>Get in Touch</h2>
-          <div className='contact-description__inner'>
+      <main id="main-content" tabIndex="-1" className="wrapper">
+        <section className="contact-description">
+          <h2 className="contact-description__headline">Get in Touch</h2>
+          <div className="contact-description__inner">
             <p>
               I’d love to hear about what you’re working on and how I could
               help. I’m currently looking for a new role and am open to a wide
@@ -26,33 +27,33 @@ export default function Home () {
               check out my online profiles below and get in touch using the
               form.
             </p>
-            <div className='professional-icons'>
-              <ul role='list'>
+            <div className="professional-icons">
+              <ul role="list">
                 <li>
-                  <a href='https://github.com' title='GitHub'>
+                  <a href="https://github.com" title="GitHub">
                     <Image
-                      src='/images/icons/github.svg'
-                      alt=''
+                      src="/images/icons/github.svg"
+                      alt=""
                       width={25}
                       height={24}
                     />
                   </a>
                 </li>
                 <li>
-                  <a href='https://twitter.com' title='Twitter'>
+                  <a href="https://twitter.com" title="Twitter">
                     <Image
-                      src='/images/icons/twitter.svg'
-                      alt=''
+                      src="/images/icons/twitter.svg"
+                      alt=""
                       width={24}
                       height={20}
                     />
                   </a>
                 </li>
                 <li>
-                  <a href='https://www.linkedin.com' title='LinkedIn'>
+                  <a href="https://www.linkedin.com" title="LinkedIn">
                     <Image
-                      src='/images/icons/linkedin.svg'
-                      alt=''
+                      src="/images/icons/linkedin.svg"
+                      alt=""
                       width={24}
                       height={24}
                     />
@@ -62,53 +63,10 @@ export default function Home () {
             </div>
           </div>
         </section>
-        <section className='contact-form'>
-          <h2 className='contact-form__headline'>Contact Me</h2>
-          <div className='contact__form'>
-            <form>
-              <div className='form__control'>
-                <label htmlFor='fullname'>Name</label>
-                <input
-                  className='form__input'
-                  type='text'
-                  name='fullname'
-                  placeholder='Jane Appleseed'
-                  id='fullname'
-                  autoComplete='name'
-                  aria-required='true'
-                  required
-                />
-              </div>
-              <div className='form__control'>
-                <label htmlFor='email'>Email Address</label>
-                <input
-                  className='form__input'
-                  type='email'
-                  name='email'
-                  placeholder='email@example.com'
-                  id='email'
-                  autoComplete='email'
-                  aria-required='true'
-                  required
-                />
-              </div>
-              <div className='form__control'>
-                <label htmlFor='message'>Message</label>
-                <textarea
-                  className='form__input'
-                  name='message'
-                  id='message'
-                  cols='20'
-                  rows='3'
-                  placeholder='How can I help?'
-                  aria-required='true'
-                  required
-                />
-              </div>
-              <button type='submit' className='button button-primary'>
-                Send Message
-              </button>
-            </form>
+        <section className="contact-form">
+          <h2 className="contact-form__headline">Contact Me</h2>
+          <div className="contact__form">
+            <ContactForm />
           </div>
         </section>
       </main>
